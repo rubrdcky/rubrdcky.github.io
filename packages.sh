1b5f2639a2c379a9d001c27905a95bf2a7badd08
rubrdcky <jwarren4881@gmail.com>
2015-12-07 06:28:09 -0500

    file restructure

--- packages.sh
+++ packages.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/Package debs
-dpkg-deb -bZgzip projects/rubrdckytest debs
-dpkg-deb -bZgzip projects/SpaceGrayRespring debs
-dpkg-deb -bZgzip projects/BlackWelcomeRespring debs
+dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/SpaceGrayRespring debs
+dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/BigApple debs
