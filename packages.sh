959030254844ad7924bbe9d2d4fb94c24c7044de
rubrdcky <jwarren4881@gmail.com>
2015-12-12 08:50:10 -0500

    Added C8

--- packages.sh
+++ packages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/SpaceGrayRespring debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/BigApple debs
+dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/C8StatusBar debs
