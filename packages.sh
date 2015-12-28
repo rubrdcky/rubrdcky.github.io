e3fc28ef19ab75c24e460ebf2435286a21f218b5
rubrdcky <jwarren4881@gmail.com>
2015-12-28 18:51:08 -0500

    initial commit

--- packages.sh
+++ packages.sh
@@ -4,4 +4,4 @@ dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/BigApple debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/C8StatusBar debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/Circle8 debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/GreatMeadow debs
-dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/SpaceGrayWatchRespring debs
+dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/AWSGRespring debs
