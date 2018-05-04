cc8deb06d85ff6d3d57e084429ac3af22343f0e1
rubrdcky <jwarren4881@gmail.com>
2018-05-04 06:55:37 -0400

    reverted back to nolabels_1.0.1

--- packages.sh
+++ packages.sh
@@ -7,4 +7,3 @@ dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/GreatMeadow debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/AWSGRespring debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/BinaryKeypad debs
 dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/HomeGesture debs
-dpkg-deb -bZgzip /Users/Jake/Documents/GitHub/Projects/nolabels debs
